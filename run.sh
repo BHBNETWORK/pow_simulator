e20481f7453ee40768f4ff4d6cb8ad19ba912c6f
gianlucamazza <gianluca@bhb.network>
2017-08-29 00:58:29 +0200

    codeclean

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# define genesis block
+# clean folder and define genesis block
+rm block_*.txt
 HEIGHT=0
 DIFFICULTY=0
 
@@ -9,24 +10,35 @@ while true; do
 
     # output to terminal current block height
     echo "Current block height: $HEIGHT"
+    echo "Current difficulty: $DIFFICULTY"
 
     # define block filename
     FILENAME="block_"$HEIGHT".txt"
 
     VARIABLENAME="test$HEIGHT"
     
-    # search for POW
+    # search for POW and measure time
     # hashcash --help for list of options
-    time hashcash -P -b $DIFFICULTY -m -r $VARIABLENAME > $FILENAME
 
-    # write new block to file
-    cat $FILENAME | tr -d "\n" | shasum | tee -a $FILENAME
+    START_TIME=$SECONDS
+    OUTPUT=$(hashcash -P -b $DIFFICULTY -m -r $VARIABLENAME)
+    ELAPSED_TIME=$(($SECONDS - $START_TIME))
+        
+    # announce new block
+    echo "BLOCK FOUND!!!"
+    echo "Elapsed time:" $ELAPSED_TIME "SECONDS"
 
+    # write hash to file
+    HEADER1=$(echo $OUTPUT)
+    HEADER2=$(echo $OUTPUT | shasum)
+    
+    echo $HEADER1 >> $FILENAME
+    echo $HEADER2 >> $FILENAME
+    
     # define next block height
     let HEIGHT=HEIGHT+1
-    
-    # increase difficulty
     let DIFFICULTY=DIFFICULTY+1
+
 done
 
 
